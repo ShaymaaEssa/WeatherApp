f4093b1692b25a7dcfe1a3e1078be45bd97c5214
ShaymaaEssa <flowrathesky@gmail.com>
2025-04-18 01:04:09 +0200

    navbar-hamburger-icon

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -9,4 +9,9 @@ import { RouterLink } from '@angular/router';
 })
 export class NavbarComponent {
 
+  isMenuOpen: boolean = false;
+
+  toggleNavbarMenu(){
+    this.isMenuOpen = !this.isMenuOpen
+  }
 }
